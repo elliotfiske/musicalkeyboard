33ee708dfbfefea2e807dce4452fd626de5a85b7
elliotfiske <elliotfiske@gmail.com>
2016-03-11 12:36:07 -0800

    Wiping out old repeat-timer thing

--- js/key_listener.js
+++ js/key_listener.js
@@ -1,14 +1,23 @@
 document.addEventListener('keydown', onKeyDown, false);
+document.addEventListener('keyup',   onKeyUp, false);
 
-var pending_keys = [];
+var pushed_keys = [];
 var last_time = 0;
 
 // # of milliseconds since we burned through all the keypresses
 var elapsed_ms = 0;
-
 var MS_BETWEEN_BEATS = 100;
 
+// Put a key in the "down" array, if it's not there already
 function onKeyDown(event) {
+   event = event || window.event;
+   var key_code = event.keyCode;
+
+   pushed_keys.push(key_code);
+}
+
+// Remove a key from the "down" array.
+function onKeyUp(event) {
    event = event || window.event;
    var key_code = event.keyCode;
 
@@ -16,33 +25,33 @@ function onKeyDown(event) {
 }
 
 function processPendingKeys() {
-	var curr_time = new Date().getTime();
-	elapsed_ms += (curr_time - last_time);
+   var curr_time = new Date().getTime();
+   elapsed_ms += (curr_time - last_time);
 
-	if (elapsed_ms > MS_BETWEEN_BEATS) {
-		for (var i = 0; i < pending_keys.length; i++) {
-			doInstrument(pending_keys[i]);
-		}
+   if (elapsed_ms > MS_BETWEEN_BEATS) {
+      for (var i = 0; i < pending_keys.length; i++) {
+         doInstrument(pending_keys[i]);
+      }
 
-		elapsed_ms = 0;
-		pending_keys = [];
-	}
+      elapsed_ms = 0;
+      pending_keys = [];
+   }
 
-	last_time = curr_time;
+   last_time = curr_time;
 }
 
 function doInstrument(key_code) {
    if (key_code == 65) { // a
-       cube1.bounceTime = 27;
-       do_sound("cube1");
+     cube1.bounceTime = 27;
+     do_sound("cube1");
    } else if (key_code == 83) { // s
-       cube2.bounceTime = 27;
-       do_sound("cube2");
+     cube2.bounceTime = 27;
+     do_sound("cube2");
    } else if (key_code == 68) { // d
-       cube3.bounceTime = 27;
-       do_sound("cube3");
+     cube3.bounceTime = 27;
+     do_sound("cube3");
    } else if (key_code == 70) { // f
-       cube4.bounceTime = 27;
-       do_sound("cube5");
-   }
+     cube4.bounceTime = 27;
+     do_sound("cube5");
+  }
 }
